b701b26241e1949a446aab0d5f98e66dc02326ad
mstefanov <martin.stefanov@premium-minds.com>
2025-05-22 10:59:10 +0100

    Feature3-child - 1st Commit

--- new.js
+++ new.js
@@ -9,6 +9,9 @@ console.log('MAIN ADVANCED');
 console.log('Feature3 - 1st Commit');
 console.log('Feature3 - 2nd Commit');
 
+console.log('Feature3-child - 1st Commit');
+
+
 
 console.log('MAIN ADVANCED MOOORE');
 
